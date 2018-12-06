fbaf091c47c319872b867d0c6cd09bc2f9b403f6
waz <daniel.alleyn@gmail.com>
2018-12-06 15:36:10 -0800

    add margin for grand total

--- src/InvoicePrinter.php
+++ src/InvoicePrinter.php
@@ -494,7 +494,6 @@ class InvoicePrinter extends FPDF
         if (!isset($this->productsEnded)) {
             $width_other = ($this->document['w'] - $this->margins['l'] - $this->margins['r'] - $this->firstColumnWidth - ($this->columns * $this->columnSpacing)) / ($this->columns - 1);
             $this->SetTextColor(50, 50, 50);
-            $this->Ln(2);
             $this->SetFont($this->font, 'B', 9);
             $this->Cell(1, 10, '', 0, 0, 'L', 0);
             $this->Cell($this->firstColumnWidth, 10, iconv("UTF-8", "ISO-8859-1//TRANSLIT", mb_strtoupper($this->lang['product'], 'UTF-8')),
@@ -517,7 +516,7 @@ class InvoicePrinter extends FPDF
             }
             $this->Cell($this->columnSpacing, 10, '', 0, 0, 'L', 0);
             $this->Cell($width_other, 10, iconv("UTF-8", "ISO-8859-1//TRANSLIT", mb_strtoupper($this->lang['total'], 'UTF-8')), 0, 0, 'C', 0);
-            $this->Ln();
+       
             $this->SetLineWidth(0.3);
             $this->SetDrawColor($this->color[0], $this->color[1], $this->color[2]);
             $this->Line($this->margins['l'], $this->GetY(), $this->document['w'] - $this->margins['r'], $this->GetY());
