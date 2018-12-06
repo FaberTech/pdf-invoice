80860db59e78d391a74ec50406e33258f63b4b08
waz <daniel.alleyn@gmail.com>
2018-12-05 16:15:07 -0800

    add totals to bottom

--- src/InvoicePrinter.php
+++ src/InvoicePrinter.php
@@ -582,7 +582,8 @@ class InvoicePrinter extends FPDF
 
         $this->Ln(5);
         $this->SetLineWidth(0.4);
-        $this->Line($this->margins['l'], $this->GetY(), $this->margins['l'] + $width + $width, $this->GetY());
+        $this->Line($this->margins['l'], $this->GetY(), $this->margins['l'] + $width, $this->GetY());
+        $this->Ln(7);
 
         //Add totals
         if ($this->totals) {
