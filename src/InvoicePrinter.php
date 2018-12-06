a5d2f786266d63fc013a0045b92e5e5df6b44f5b
waz <daniel.alleyn@gmail.com>
2018-12-06 15:23:52 -0800

    add margin for grand total

--- src/InvoicePrinter.php
+++ src/InvoicePrinter.php
@@ -494,7 +494,7 @@ class InvoicePrinter extends FPDF
         if (!isset($this->productsEnded)) {
             $width_other = ($this->document['w'] - $this->margins['l'] - $this->margins['r'] - $this->firstColumnWidth - ($this->columns * $this->columnSpacing)) / ($this->columns - 1);
             $this->SetTextColor(50, 50, 50);
-            $this->Ln(1);
+            $this->Ln(2);
             $this->SetFont($this->font, 'B', 9);
             $this->Cell(1, 10, '', 0, 0, 'L', 0);
             $this->Cell($this->firstColumnWidth, 10, iconv("UTF-8", "ISO-8859-1//TRANSLIT", mb_strtoupper($this->lang['product'], 'UTF-8')),
@@ -551,15 +551,13 @@ class InvoicePrinter extends FPDF
         $this->Cell($width, $lineheight, $title, 0, 0, 'L');
         $this->SetFont($this->font, '', 8);
         $this->SetTextColor(100, 100, 100);
-        $this->Ln(7);
+        $this->Ln(2);
         // project supervisor
         $this->Cell($width, $lineheight, iconv("UTF-8", "ISO-8859-1//TRANSLIT", 'Supervisor: '.$section['supervisor']), 0, 0, 'L');
-        $this->Ln(5);
+        $this->Ln(2);
         $this->SetLineWidth(0.4);
         $this->SetDrawColor($this->color[0], $this->color[1], $this->color[2]);
         $this->Line($this->margins['l'], $this->GetY(), $this->margins['l'] + $width, $this->GetY());
-
-        $this->Ln(1);
 
     }
 
