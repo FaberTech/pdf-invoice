82230fbae217ab59c28c67d4683d03ea865d3f33
waz <daniel.alleyn@gmail.com>
2018-12-06 11:39:12 -0800

    format

--- src/InvoicePrinter.php
+++ src/InvoicePrinter.php
@@ -572,7 +572,7 @@ class InvoicePrinter extends FPDF
         $this->Ln(5);
         $this->SetTextColor(50, 50, 50);
         $this->SetFont($this->font, 'B', 10);
-        $this->Cell(0, $lineheight, iconv("UTF-8", "ISO-8859-1//TRANSLIT", $this->lang['grand_total']), 0, 0, 'L');
+        $this->Cell(0, $lineheight, iconv("UTF-8", "ISO-8859-1//TRANSLIT", $this->lang['grand_total']), 0, 0, 'R');
         $this->Ln(7);
         $this->SetLineWidth(0.4);
         $this->Line($this->margins['l'] + $width, $this->GetY(), $this->margins['l'] + $width + $width,
